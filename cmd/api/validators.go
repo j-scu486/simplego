b2bbf07ecabe9c476bbe02ce7f9a43da056b2ee9
Joseph Scutella <joseph@yaraku.com>
2024-05-29 09:13:01 +0900

    store create validator

--- cmd/api/validators.go
+++ cmd/api/validators.go
@@ -7,10 +7,28 @@ import (
 )
 
 type Item struct {
-	Name     string  `validate:"required, max=40"`
-	Price    float64 `validate:"required, numeric, gt=0"`
-	Quantity int     `validate:"required, number"`
-	OnSale   bool    `validate:"required, boolean"`
+	Name     string  `validate:"required,max=40"`
+	Price    float64 `validate:"required,numeric,gt=0"`
+	Quantity int     `validate:"required,number"`
+	OnSale   bool    `validate:"required,boolean"`
+}
+
+type Store struct {
+	Name  string `validate:"required,max=40"`
+	Owner string `validate:"required,oneof='state' 'private'"`
+}
+
+func validateStore(s *Store) error {
+	var validate = validator.New(validator.WithRequiredStructEnabled())
+
+	err := validate.Struct(s)
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	return nil
 }
 
 func validateItem(i *Item) error {
