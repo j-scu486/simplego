65df1e78dde842d6e3521d2bb09c51bd4a1caa52
Joseph Scutella <joseph@yaraku.com>
2024-05-30 12:56:04 +0900

    add back header

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -40,6 +40,7 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data any, h
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
 	w.Write(js)
 
 	return nil
